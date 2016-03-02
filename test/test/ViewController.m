db920ea2799ba5c28abc73afcd235079e526260c
玲玲 <suxl@shicaidai.com>
2016-03-02 16:25:39 +0800

    定义test3方法

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -91,6 +91,11 @@
     [cLabel sizeToFit];
     [self.view addSubview:cLabel];
 }
+
+- (void)test3
+{
+    NSLog(@"我是test3");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
