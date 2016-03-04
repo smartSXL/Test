1f4b83ce01beb4abff9beccbb8f32485ff2aa641
玲玲 <suxl@shicaidai.com>
2016-03-04 17:44:38 +0800

    去除不必要的函数

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -98,9 +98,4 @@
     [self.otherBtn addSubview:btn];
     NSLog(@"我是test3");
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
