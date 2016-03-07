878303e3d34b273b9fe40aeee9388ca7b963aa33
玲玲 <suxl@shicaidai.com>
2016-03-07 10:35:41 +0800

    添加自定义按钮文件

---
+++ test/test/DIYButton.h
@@ -0,0 +1,13 @@
+//
+//  DIYButton.h
+//  test
+//
+//  Created by 001号 on 16/3/7.
+//  Copyright © 2016年 001号. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIYButton : UIButton
+
+@end
---
+++ test/test/DIYButton.m
@@ -0,0 +1,13 @@
+//
+//  DIYButton.m
+//  test
+//
+//  Created by 001号 on 16/3/7.
+//  Copyright © 2016年 001号. All rights reserved.
+//
+
+#import "DIYButton.h"
+
+@implementation DIYButton
+
+@end
